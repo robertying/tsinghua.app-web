1fdf5742c297f161b161adb4cf2238389b6a6cc4
Rui Ying <yingrui205@gmail.com>
2021-03-30 14:55:40 +0800

    Add 500 page

---
+++ pages/500.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import { NextSeo } from "next-seo";
+import { Button, Container, Paper, Typography } from "@material-ui/core";
+
+const InternalError: React.FC = () => {
+  return (
+    <>
+      <NextSeo title="500 Internal Error" noindex />
+      <Container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "100vh",
+        }}
+        maxWidth="sm"
+      >
+        <Paper
+          sx={{
+            p: 4,
+          }}
+        >
+          <Typography sx={{ fontWeight: "bold" }} variant="h4" component="h1">
+            500 Internal Error
+          </Typography>
+          <Typography sx={{ mt: 4 }} variant="body1">
+            服务器内部产生了错误。请稍后重试。
+          </Typography>
+          <Link href="/">
+            <a>
+              <Button sx={{ mt: 2 }} variant="contained">
+                返回主页
+              </Button>
+            </a>
+          </Link>
+        </Paper>
+      </Container>
+    </>
+  );
+};
+
+export default InternalError;
