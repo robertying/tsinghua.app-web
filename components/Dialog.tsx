4d1a994209a2aaff077a46ef9df9729a066a1b6a
Rui Ying <yingrui205@gmail.com>
2021-05-04 22:47:56 +0800

    Clean MyDialog

--- components/Dialog.tsx
+++ components/Dialog.tsx
@@ -12,9 +12,9 @@ import {
   Tooltip,
   Typography,
 } from "@material-ui/core";
-import { Close } from "@material-ui/icons";
 import { useTheme } from "@material-ui/core/styles";
 import { TransitionProps } from "@material-ui/core/transitions";
+import { Close } from "@material-ui/icons";
 
 const DialogTransition = forwardRef(function DialogTransition(
   props: TransitionProps & { children?: React.ReactElement },
@@ -24,7 +24,7 @@ const DialogTransition = forwardRef(function DialogTransition(
 });
 
 export interface MyDialogProps extends DialogProps {
-  onClose?: (event: {}, reason?: "backdropClick" | "escapeKeyDown") => void;
+  onClose?: () => void;
   onOk?: () => void;
   okText: string;
   okLoading?: boolean;
@@ -32,20 +32,31 @@ export interface MyDialogProps extends DialogProps {
 }
 
 const MyDialog: React.FC<MyDialogProps> = ({
+  onClose,
   onOk,
   okText,
   okLoading,
   title,
+  children,
   ...restProps
 }) => {
   const theme = useTheme();
   const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const handleClose: DialogProps["onClose"] = (e, reason) => {
+    if (reason) {
+      return;
+    }
+
+    onClose?.();
+  };
 
   return (
     <Dialog
       fullScreen={fullScreen}
       fullWidth
       TransitionComponent={fullScreen ? DialogTransition : undefined}
+      onClose={handleClose}
       {...restProps}
     >
       <Box
@@ -59,18 +70,18 @@ const MyDialog: React.FC<MyDialogProps> = ({
         component="nav"
       >
         <Tooltip title="关闭" placement="right">
-          <IconButton onClick={restProps.onClose}>
+          <IconButton onClick={onClose}>
             <Close />
           </IconButton>
         </Tooltip>
         <Typography sx={{ fontWeight: "bold" }}>{title}</Typography>
         {okLoading ? (
-          <CircularProgress size="2rem" />
+          <CircularProgress sx={{ mr: 1 }} size="1.5rem" />
         ) : (
           <Button onClick={onOk}>{okText}</Button>
         )}
       </Box>
-      <DialogContent>{restProps.children}</DialogContent>
+      <DialogContent>{children}</DialogContent>
     </Dialog>
   );
 };
