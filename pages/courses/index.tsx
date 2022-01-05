93ab35ca4485b304157192f7c46d88b1d3295820
Rui Ying <yingrui205@gmail.com>
2022-01-04 17:08:34 -0800

    Change current semester to 2021-2022-2

--- pages/courses/index.tsx
+++ pages/courses/index.tsx
@@ -8,7 +8,7 @@ import { getSemesterTextFromId } from "lib/format";
 import { GetCourseCount, GetCourseCountVariables } from "api/types";
 import { GET_COURSE_COUNT } from "api/course";
 
-const CURRENT_SEMESTER_ID = "2021-2022-1";
+const CURRENT_SEMESTER_ID = "2021-2022-2";
 
 interface CourseHomeProps {
   courseCount?: number | null;
